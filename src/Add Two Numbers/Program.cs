ad7252c875b8db13243ab3b1c5410db63ec3c6e1
ShadowD1337 <martin.jechkov@gmail.com>
2023-12-05 14:10:28 +0200

    Solve Add Two Numbers

--- src/Add Two Numbers/Program.cs
+++ src/Add Two Numbers/Program.cs
@@ -5,20 +5,30 @@ internal class Program
     static void Main(string[] args)
     {
         var result = AddTwoNumbers(
-            new ListNode(2, new ListNode(4, new ListNode(3))),
-            new ListNode(5, new ListNode(6, new ListNode(4))));
+            //new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))),
+            //new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))));
+            //new ListNode(2, new ListNode(4, new ListNode(3))),
+            //new ListNode(5, new ListNode(6, new ListNode(4))));
+            new ListNode(0),
+            new ListNode(7, new ListNode(3)));
     }
     public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
         int carryNum = 0;
         var resultNode = new ListNode();
         var nextNode = resultNode;
-        var currentNode1 = l1;
-        var currentNode2 = l2;
-        while (currentNode1 != null || currentNode2 != null)
+        ListNode? currentNode1 = l1;
+        ListNode? currentNode2 = l2;
+        while (true)
         {
+            if (currentNode1 is null && currentNode2 is null && carryNum == 0)
+            {
+                break;
+            }
+
             int currentSum = (currentNode1 is null ? 0 : currentNode1.val) + (currentNode2 is null ? 0 : currentNode2.val);
             currentSum += carryNum;
+            carryNum = 0;
             if (currentSum >= 10)
             {
                 carryNum = 1;
@@ -28,11 +38,59 @@ internal class Program
             {
                 nextNode.val = currentSum;
             }
-            nextNode.next = new ListNode();
-            nextNode = nextNode.next;
 
-            currentNode1 = currentNode1.next;
-            currentNode2 = currentNode2.next;
+            if (currentNode1 is null && currentNode2 is null)
+            {
+                break;
+            }
+
+            if (currentNode1 is null && currentNode2.next != null)
+            {
+                nextNode.next = new ListNode();
+                nextNode = nextNode.next;
+            }
+            else if (currentNode2 is null && currentNode1.next != null)
+            {
+                nextNode.next = new ListNode();
+                nextNode = nextNode.next;
+            }
+            else if (carryNum > 0)
+            {
+                nextNode.next = new ListNode();
+                nextNode = nextNode.next;
+            }
+            else {
+                var added = false;
+                if (currentNode1 != null && !added)
+                {
+                    if (currentNode1.next != null)
+                    {
+                        nextNode.next = new ListNode();
+                        nextNode = nextNode.next;
+                        added = true;
+                    }
+                }
+                if (currentNode2 != null && !added)
+                {
+                    if (currentNode2.next != null)
+                    {
+                        nextNode.next = new ListNode();
+                        nextNode = nextNode.next;
+                        added = true;
+                    }
+                }
+            }
+
+
+            if (currentNode1 != null)
+            {
+                currentNode1 = currentNode1.next;
+            }
+
+            if (currentNode2 != null)
+            {
+                currentNode2 = currentNode2.next;
+            }
         }
 
         return resultNode;
