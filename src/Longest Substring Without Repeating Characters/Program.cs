6cfb8f9817285e48ad77fecbcfd5c0c4426f5264
ShadowD1337 <martin.jechkov@gmail.com>
2023-12-05 16:05:26 +0200

    Longest Substring Without Repeating Characters

---
+++ src/Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LongestSubstringWithoutRepeatingCharacters;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var answer = LengthOfLongestSubstring("abcabcbb");
+        Console.WriteLine(answer);
+    }
+    public static int LengthOfLongestSubstring(string s)
+    {
+        int biggestLength = 0;
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            var usedChars = new List<char>();
+            for(int j = i; j < s.Length; j++)
+            {
+                if (!usedChars.Contains(s[j]))
+                {
+                    usedChars.Add(s[j]);
+                }
+                else break;
+            }
+            biggestLength = Math.Max(biggestLength, usedChars.Count);
+        }
+
+        return biggestLength;
+    }
+}
