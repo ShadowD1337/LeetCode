cc0da4055fe54ed069cea6d9483e20cddbe9cda2
ShadowD1337 <martin.jechkov@gmail.com>
2023-12-06 16:16:09 +0200

    Zigzag Conversion

---
+++ src/Longest Palindromic Substring/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace LongestPalindromicSubstring;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(LongestPalindrome("aba"));
+    }
+    public static string LongestPalindrome(string s)
+    {
+        var palindromes = new HashSet<string>();
+        var maxLength = 0;
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if (s.Length - i + 1 <= maxLength) break;
+            for(int j = s.Length; j >= i; j--)
+            {
+                if (j - i + 1 <= maxLength) break;
+
+                var substring = s.Substring(i, j - i);
+                if (IsPalindrome(substring))
+                {
+                    palindromes.Add(substring);
+                    maxLength = substring.Length;
+                }
+            }
+        }
+
+        // Fast method for finding longest string in a List
+        return palindromes.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur);
+    }
+
+    public static bool IsPalindrome(string input)
+    {
+        if (input.Length == 1) return true;
+
+        for (int i = 0; i < input.Length / 2; i++)
+        {
+            if (input[i] != input[input.Length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/Zigzag Conversion/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace ZigzagConversion;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Convert("AB", 3) == "");
+    }
+    public static string Convert(string s, int numRows)
+    {
+        if (s.Length <= 2 || numRows == 1) return s;
+        // Cant figure out how to calculate matrix length. Doesnt make a big difference in this case
+        var matrix = new char[s.Length, numRows];
+        var counterVertical = 0;
+        var counterHorizontal = 0;
+        var reached = false;
+
+        matrix[0, 0] = s[0];
+        for (int i = 1; i < s.Length; i++)
+        {
+            if(counterVertical < numRows && !reached)
+            {
+                counterVertical++;
+                matrix[counterHorizontal, counterVertical] = s[i];
+                if (counterVertical == numRows - 1) reached = true;
+            }
+            else
+            {
+                counterVertical--;
+                counterHorizontal = counterHorizontal == matrix.GetLength(0) - 1 ? counterHorizontal : counterHorizontal + 1;
+                matrix[counterHorizontal, counterVertical] = s[i];
+                if(counterVertical == 0 || counterHorizontal == matrix.GetLength(0) - 1) reached = false;
+            }
+        }
+
+        var resultSb = new StringBuilder();
+
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for(int j = 0; j < matrix.GetLength(0); j++)
+            {
+                if (matrix[j,i] != '\0')
+                {
+                    resultSb.Append(matrix[j, i]);
+                }
+            }
+        }
+
+        return resultSb.ToString();
+    }
+}
